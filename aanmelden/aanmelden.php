9ec8f441ba533cc4d682d27442daee7cb2a2e4d3
Anan6 <anan6@Debian.anan6.com>
2018-10-24 14:46:53 +0200

    Placeholders veranderd (Parsa)

--- aanmelden/aanmelden.php
+++ aanmelden/aanmelden.php
@@ -26,7 +26,7 @@ function fDNAanmelden($atts, $content = '', $tag){
     $html .= '<label><input type="radio" name="wie" value="M">Mijzelf</label>';
     $html .= '<label><input type="radio" name="wie" value="A">Iemand die ik begeleid</label></div></div>';
     $html .= '<div class="entry">';
-    $html .= '<label class="red" for="aanEmail">E-mailadres deelnemer:</label><input type="text" name="email" id="dnEmail" placeholder="Email" class="required formatEmail"><button id="dnSubmit" type="submit" disabled="disabled">Aanmelden</button></div>';
+    $html .= '<label class="red" for="aanEmail">E-mailadres deelnemer:</label><input type="text" name="email" id="dnEmail" placeholder="Vul hier uw email in" class="required formatEmail"><button id="dnSubmit" type="submit" disabled="disabled">Aanmelden</button></div>';
     $html .= '<p id="dnMessage"></p></form>';
     return $html;
 }
@@ -42,7 +42,7 @@ add_shortcode('vwAanmelden', 'fVWAanmelden');
     $html .= '<div class="entry">';
     $html .= '<div>';
     $html .= '<div class="entry">';
-    $html .= '<input type="text" name="email" id="vwEmail" placeholder="Voer hier je email in" class="required formatEmail">';
+    $html .= '<input type="text" name="email" id="vwEmail" placeholder="Voer hier uw email in" class="required formatEmail">';
     $html .= '<label for="vwAkkoord"><input type="checkbox" id="vwAkkoord" name="vwAkkoord" value="OK">Ik heb de <a href="#">Privacy voorwaarden</a> gelezen en ga hiermee akkoord</label><button id="vwSubmit" type="submit" disabled="disabled">Aanmelden</button></div>';
     $html .= '<p id="vwMessage"></p></form>';
     return $html;
