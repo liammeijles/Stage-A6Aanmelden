92292fb4ae31eced1f7a04820b089735f07661ed
Anan6 <anan6@Debian.anan6.com>
2018-10-26 09:27:08 +0200

    Spellingsfout eruit gehaald

--- aanmelden/vwAanmelden.php
+++ aanmelden/vwAanmelden.php
@@ -338,7 +338,7 @@ try{
     $oInput = $oPage->getElementById('id0213');
     $oInput->setAttribute('value', $aShow['vrijwilliger']['email']);
 
-    /*** AANVULLENDE INFOMATIE ***/
+    /*** AANVULLENDE INFORMATIE ***/
     $oDiv = $oPage->getElementById('para020101');
     $cInputs = $oDiv->getElementsByTagName('input');
     $aBeschikbaar = json_decode($aShow['vrijwilliger']['dossier']['beschikbaar']);
